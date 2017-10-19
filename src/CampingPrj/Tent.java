04588410c2facd7d7afafc5862d119dd77a86d59
bthedorff <32941719+bthedorff@users.noreply.github.com>
2017-10-19 16:46:00 -0400

    Adds code to Tent
    getter, setter, constructor

--- src/CampingPrj/Tent.java
+++ src/CampingPrj/Tent.java
@@ -1,5 +1,21 @@
 package CampingPrj;
 
 public class Tent extends Site{
+	/** Represents the number of tenters on this site */
+	private int numOfTenters;
+
+	public Tent() {
+		numOfTenters = 0;
+	}
+	// add constructor
+	// add getter, setter methods
+	public int getNumOfTenters() {
+		return numOfTenters;
+	}
+
+	public void setNumOfTenters(int numOfTenters) {
+		this.numOfTenters = numOfTenters;
+	}
+
 
 }
