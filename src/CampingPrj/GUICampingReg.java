0f3cd8c2e077acb4017c9c3af2aa637941c97787
Nate Johnson <natej1@live.com>
2017-10-19 17:05:54 -0400

    Added comments and code bit to GUICampingReg

--- src/CampingPrj/GUICampingReg.java
+++ src/CampingPrj/GUICampingReg.java
@@ -6,13 +6,19 @@ import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 
 public class GUICampingReg extends JFrame implements ActionListener{
+	   // declare GUI components (menu items, buttons, etc.) needed
+	   // constructor method that prepares the GUI
+	   // event listeners and other methods needed to build the GUI
+
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// This does not work now but it will be needed later i guess
-//		if (resvRVItem == comp) {
-//			RV t = new RV();
-//			DialogCheckInRv x = new DialogCheckInRv(this, t);
+		// Finally, to invoke this dialog box from the GUICampingReg class, the following code may help. 
+		Object comp = e.getSource();
+		Object resvRVItem;
+		if (resvRVItem == comp) {
+			RV t = new RV(0);
+			DialogCheckInRv x = new DialogCheckInRv(this, t);
 
 	}
 
