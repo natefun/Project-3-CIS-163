87daa88594698cc8c2ff871b5f8d4d1769ac167f
DenverDeBoer <32941698+DenverDeBoer@users.noreply.github.com>
2017-10-19 17:57:54 -0400

    DialogCheckInTent code

--- src/CampingPrj/DialogCheckInTent.java
+++ src/CampingPrj/DialogCheckInTent.java
@@ -1,5 +1,6 @@
 package CampingPrj;
 
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -7,6 +8,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class DialogCheckInTent extends JDialog implements ActionListener{
@@ -14,26 +16,38 @@ public class DialogCheckInTent extends JDialog implements ActionListener{
 	private JTextField OccupyedOnTxt;
 	private JTextField stayingTxt;
 	private JTextField siteNumberTxt;
-	private JComboBox powerTxt;
+	private JTextField numOfGuests;
 	private JButton okButton;
 	private JButton cancelButton;
 	private boolean closeStatus;
 	private Site unit;
+	private JPanel panel;
 	
 	public DialogCheckInTent(JFrame paOccupy, Site d) {
 		nameTxt = new JTextField("John Smith");
 		OccupyedOnTxt = new JTextField("03/14/2015");
 		stayingTxt = new JTextField("7");
 		siteNumberTxt = new JTextField("1");
-		powerTxt = new JComboBox();	//Subject to change
+		numOfGuests = new JTextField("1");
 		okButton = new JButton("OK");
 		cancelButton = new JButton("Cancel");
 		closeStatus = false;
 		unit = d;
+		
+		paOccupy = new JFrame("Reserve an Tent site");
+		paOccupy.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		paOccupy.getContentPane().add(createPanel());
+		paOccupy.setSize(300, 500);
+		paOccupy.setVisible(true);
+	}
+	
+	private JPanel createPanel() {
+		panel = new JPanel();
+		panel.setLayout(new GridLayout(6, 1));
+		return panel;
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
 	}
 }
