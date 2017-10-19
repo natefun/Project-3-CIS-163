47f942946cb8110370f0a155c4a117cffccd2bc5
DenverDeBoer <32941698+DenverDeBoer@users.noreply.github.com>
2017-10-19 16:50:10 -0400

    RV class setters and getters

--- src/CampingPrj/RV.java
+++ src/CampingPrj/RV.java
@@ -1,5 +1,45 @@
 package CampingPrj;
 
 public class RV extends Site{
-
+	/** holds the amount of power requested for each RV site */
+	private int power;		//30, 40, 50 amps
+	
+	/******************************************************************
+	 * Constructor
+	 * Initializes the instance variable
+	 * @param p holds the amount of power to the RV site
+	 *****************************************************************/
+	public RV(int p) {
+		power = verifyPower(p);
+	}
+	
+	/******************************************************************
+	 * Sets the amount of power to the RV site
+	 * @param p holds the amount of power to the RV site
+	 *****************************************************************/
+	public void setPower(int p) {
+		power = verifyPower(p);
+	}
+	
+	/******************************************************************
+	 * Returns the amount of power to the RV site
+	 * @return the amount of power to the RV site
+	 *****************************************************************/
+	public int getPower() {
+		return power;
+	}
+	
+	/******************************************************************
+	 * Returns the amount of power to the RV site. Valid inputs are
+	 * 30, 40, and 50 amps. Other input will be invalid and will
+	 * return 0.
+	 * @param p holds the amount of power at the RV site
+	 * @return either the amount provided or 0
+	 *****************************************************************/
+	public int verifyPower(int p) {
+		if(p == 30 || p == 40 || p == 50)
+			return p;
+		else
+			return 0;
+	}
 }
