e75c0d1be18478f2abbd79824b9cf6a4d2884a2f
Nate Johnson <natej1@live.com>
2017-10-19 16:39:45 -0400

    Added code to DialogCheckInRv

--- src/CampingPrj/DialogCheckInRv.java
+++ src/CampingPrj/DialogCheckInRv.java
@@ -1,5 +1,31 @@
 package CampingPrj;
 
-public class DialogCheckInRv {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class DialogCheckInRv extends JDialog implements ActionListener{
+	private JTextField nameTxt;
+	private JTextField OccupyedOnTxt;
+	private JTextField stayingTxt;
+	private JTextField siteNumberTxt;
+	private JComboBox powerTxt;
+	private JButton okButton;
+	private JButton cancelButton;
+	private boolean closeStatus;
+	private Site unit;  	
+
+	public DialogCheckInRv(JFrame paOccupy, Site d) {	
+		unit = d; 
+//		...
+	}
+	public void actionPerformed(ActionEvent e) {
+//		...
+	}
 
 }
