e9008c7b769fca7ca08d03d7b367503e4977dfc7
DenverDeBoer <32941698+DenverDeBoer@users.noreply.github.com>
2017-10-19 16:37:26 -0400

    Implement ActionListener in DialogCheckInRV

--- src/CampingPrj/DialogCheckInRv.java
+++ src/CampingPrj/DialogCheckInRv.java
@@ -1,5 +1,14 @@
 package CampingPrj;
 
-public class DialogCheckInRv {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DialogCheckInRv implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
