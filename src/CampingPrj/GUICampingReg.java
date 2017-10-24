5245730d84d01778f8dfaeb6e89937ee59c655b0
Nate Johnson <natej1@live.com>
2017-10-24 16:30:03 -0400

    All buttons have action listeners
    every button exits now

--- src/CampingPrj/GUICampingReg.java
+++ src/CampingPrj/GUICampingReg.java
@@ -12,7 +12,7 @@ public class GUICampingReg extends JFrame implements ActionListener{
 	// event listeners and other methods needed to build the GUI
 	JMenu file, checkingIn;
 	JMenuBar menuBar;
-	JMenuItem openSerial,saveSerial, openTxt,saveTxt, exit, TentIn, RVIn; 
+	JMenuItem openSerial,saveSerial, openTxt,saveTxt, exit, tentIn, RVIn; 
 
 	public GUICampingReg() {
 		menuBar = new JMenuBar();
@@ -25,6 +25,9 @@ public class GUICampingReg extends JFrame implements ActionListener{
 		openTxt= new JMenuItem("Open Text File");
 		saveTxt= new JMenuItem("Save Text File");
 		exit = new JMenuItem("Exit");
+		
+		tentIn = new JMenuItem("Check-In Tent Site");
+		RVIn = new JMenuItem("Check-In RV Site");
 
 		menuBar.add(file);
 		menuBar.add(checkingIn);
@@ -37,11 +40,17 @@ public class GUICampingReg extends JFrame implements ActionListener{
 		file.addSeparator();
 		file.add(exit);
 		
+		checkingIn.add(tentIn);
+		checkingIn.add(RVIn);
+		
 		openSerial.addActionListener(this);
 		saveSerial.addActionListener(this);
 		openTxt.addActionListener(this);
 		saveTxt.addActionListener(this);
 		exit.addActionListener(this);
+		
+		tentIn.addActionListener(this);
+		RVIn.addActionListener(this);
 		
 		setJMenuBar(menuBar);
 //		add(menuBar);
@@ -59,6 +68,23 @@ public class GUICampingReg extends JFrame implements ActionListener{
 		if(e.getSource().equals(exit))
 			System.exit(0);
 		
+		if(e.getSource().equals(openSerial))
+			System.exit(0);
+		
+		if(e.getSource().equals(saveSerial))
+			System.exit(0);
+		
+		if(e.getSource().equals(openTxt))
+			System.exit(0);
+		
+		if(e.getSource().equals(saveTxt))
+			System.exit(0);
+		
+		if(e.getSource().equals(tentIn))
+			System.exit(0);
+		
+		if(e.getSource().equals(RVIn))
+			System.exit(0);
 		
 		// Finally, to invoke this dialog box from the GUICampingReg class, the following code may help. 
 		Object comp = e.getSource();
