8636035f04c7d88252cf46aafb4cb5347e56ea87
Nate Johnson <natej1@live.com>
2017-10-24 16:24:17 -0400

    Gui sorta works
    We have a frame and a menu bar and an exit button that works now

--- src/CampingPrj/GUICampingReg.java
+++ src/CampingPrj/GUICampingReg.java
@@ -7,30 +7,59 @@ import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 
 public class GUICampingReg extends JFrame implements ActionListener{
-	   // declare GUI components (menu items, buttons, etc.) needed
-	   // constructor method that prepares the GUI
-	   // event listeners and other methods needed to build the GUI
+	// declare GUI components (menu items, buttons, etc.) needed
+	// constructor method that prepares the GUI
+	// event listeners and other methods needed to build the GUI
+	JMenu file, checkingIn;
+	JMenuBar menuBar;
+	JMenuItem openSerial,saveSerial, openTxt,saveTxt, exit, TentIn, RVIn; 
 
+	public GUICampingReg() {
+		menuBar = new JMenuBar();
 
-//	public static void main(String[] args) {
-//		JMenu File, checkingIn;
-//		JMenuBar FileMenu;
-//		JMenuItem OpenSerial,SaveSerial, OpenTxt,SaveTxt, Exit, TentIn, RVIn; 
-//		
-//		JFrame frame = new JFrame("Camping");
-//		
-//		File = new JMenu();
-//		checkingIn= new JMenu();
-//		FileMenu = new JMenuBar();
-//		
-//		OpenSerial= new JMenuItem("Open Serializable File");
-//		SaveSerial= new JMenuItem("Save Serializable File");
-//		OpenTxt= new JMenuItem("Open Text File");
-//		SaveTxt= new JMenuItem("Save Text File");
-//		Exit = new JMenuItem("Exit");
-//	}
+		file = new JMenu("File");
+		checkingIn= new JMenu("Checking In");
+
+		openSerial= new JMenuItem("Open Serializable File");
+		saveSerial= new JMenuItem("Save Serializable File");
+		openTxt= new JMenuItem("Open Text File");
+		saveTxt= new JMenuItem("Save Text File");
+		exit = new JMenuItem("Exit");
+
+		menuBar.add(file);
+		menuBar.add(checkingIn);
+		
+		file.add(openSerial);
+		file.add(saveSerial);
+		file.addSeparator();
+		file.add(openTxt);
+		file.add(saveTxt);
+		file.addSeparator();
+		file.add(exit);
+		
+		openSerial.addActionListener(this);
+		saveSerial.addActionListener(this);
+		openTxt.addActionListener(this);
+		saveTxt.addActionListener(this);
+		exit.addActionListener(this);
+		
+		setJMenuBar(menuBar);
+//		add(menuBar);
+		
+		setVisible(true);
+		setSize(800, 600);
+	}
+
+	public static void main(String[] args) {
+		new GUICampingReg();
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		if(e.getSource().equals(exit))
+			System.exit(0);
+		
+		
 		// Finally, to invoke this dialog box from the GUICampingReg class, the following code may help. 
 		Object comp = e.getSource();
 		Object resvRVItem = null;
