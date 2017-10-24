31f6db950a3653287ebe30c609da0549919fbffd
DenverDeBoer <32941698+DenverDeBoer@users.noreply.github.com>
2017-10-24 17:02:10 -0400

    DialogCheckInTent

--- src/CampingPrj/DialogCheckInTent.java
+++ src/CampingPrj/DialogCheckInTent.java
@@ -5,18 +5,23 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class DialogCheckInTent extends JDialog implements ActionListener{
+public class DialogCheckInTent extends JDialog {
+	private JLabel name;
+	private JLabel OccupyedOn;
+	private JLabel staying;
+	private JLabel siteNumber;
+	private JLabel numOfGuests;
 	private JTextField nameTxt;
 	private JTextField OccupyedOnTxt;
 	private JTextField stayingTxt;
 	private JTextField siteNumberTxt;
-	private JTextField numOfGuests;
+	private JTextField numOfGuestsTxt;
 	private JButton okButton;
 	private JButton cancelButton;
 	private boolean closeStatus;
@@ -24,15 +29,33 @@ public class DialogCheckInTent extends JDialog implements ActionListener{
 	private JPanel panel;
 	
 	public DialogCheckInTent(JFrame paOccupy, Site d) {
+		name = new JLabel("Name:");
+		OccupyedOn = new JLabel("Occupyed on Date:");
+		staying = new JLabel("Days planning on staying:");
+		siteNumber = new JLabel("Requested site number:");
+		numOfGuests = new JLabel("Number of tenters:");
+		
 		nameTxt = new JTextField("John Smith");
 		OccupyedOnTxt = new JTextField("03/14/2015");
 		stayingTxt = new JTextField("7");
 		siteNumberTxt = new JTextField("1");
-		numOfGuests = new JTextField("1");
+		numOfGuestsTxt = new JTextField("1");
+		
 		okButton = new JButton("OK");
 		cancelButton = new JButton("Cancel");
+		
 		closeStatus = false;
 		unit = d;
+		
+		ButtonListener listener = new ButtonListener();
+		
+		nameTxt.addActionListener(listener);
+		OccupyedOnTxt.addActionListener(listener);
+		stayingTxt.addActionListener(listener);
+		siteNumberTxt.addActionListener(listener);
+		numOfGuestsTxt.addActionListener(listener);
+		okButton.addActionListener(listener);
+		cancelButton.addActionListener(listener);
 		
 		paOccupy = new JFrame("Reserve an Tent site");
 		paOccupy.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -43,11 +66,32 @@ public class DialogCheckInTent extends JDialog implements ActionListener{
 	
 	private JPanel createPanel() {
 		panel = new JPanel();
-		panel.setLayout(new GridLayout(6, 1));
+		panel.setLayout(new GridLayout(6, 2));
+		panel.add(name);
+		panel.add(nameTxt);
+		panel.add(OccupyedOn);
+		panel.add(OccupyedOnTxt);
+		panel.add(staying);
+		panel.add(stayingTxt);
+		panel.add(siteNumber);
+		panel.add(siteNumberTxt);
+		panel.add(numOfGuests);
+		panel.add(numOfGuestsTxt);
+		panel.add(okButton);
+		panel.add(cancelButton);
 		return panel;
 	}
 	
-	@Override
-	public void actionPerformed(ActionEvent e) {
+	private class ButtonListener implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == okButton) {
+				
+			}
+			else if(e.getSource() == cancelButton) {
+				
+			}
+		}
 	}
 }
