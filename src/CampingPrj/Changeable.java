af799642f31f5e1476e6dbb20fd8cf781d2ff9d9
Nate Johnson <natej1@live.com>
2017-10-31 18:20:11 -0400

    Added the crap from the internet

---
+++ src/CampingPrj/Changeable.java
@@ -0,0 +1,34 @@
+package CampingPrj;
+/**
+
+ * Interface to implement a Changeable type of action - either undo or redo.
+
+ * @author Greg COpe
+
+ *
+
+ */
+
+public interface Changeable {
+
+
+
+	/**
+
+	 * Undoes an action
+
+	 */
+
+	public void undo();
+
+	
+
+	/**
+
+	 * Redoes an action
+
+	 */
+
+	public void redo();
+
+}
