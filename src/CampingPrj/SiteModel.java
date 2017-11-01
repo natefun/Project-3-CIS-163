a0927317af71fa52b8b31eba48f3aa7849517473
Nate Johnson <natej1@live.com>
2017-11-01 15:01:45 -0400

    Displays a time in table now

--- src/CampingPrj/SiteModel.java
+++ src/CampingPrj/SiteModel.java
@@ -76,7 +76,7 @@ public class SiteModel extends AbstractTableModel implements Changeable{
 		case 0://returns name
 			return listSites.get(rowIndex).getNameReserving();
 		case 1://returns check in date
-			return listSites.get(rowIndex).getCheckIn();
+			return listSites.get(rowIndex).getCheckIn().getTime();
 		case 2://returns days staying
 			return listSites.get(rowIndex).getDaysStaying();
 		case 3://returns site number
