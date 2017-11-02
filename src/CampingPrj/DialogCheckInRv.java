703e07578c3c1cbd2d3a5692ae6a6d8681b4a4cc
DenverDeBoer <32941698+DenverDeBoer@users.noreply.github.com>
2017-11-02 13:54:27 -0400

    LeapYear bug fix

--- src/CampingPrj/DialogCheckInRv.java
+++ src/CampingPrj/DialogCheckInRv.java
@@ -161,6 +161,8 @@ public class DialogCheckInRv extends JDialog {
 	private boolean isLeapYear(int year) {
 		if(year % 4 == 0 && year % 100 != 0)
 			return true;
+		else if(year % 100 == 0)
+			return false;
 		else if(year % 400 == 0)
 			return true;
 		else
