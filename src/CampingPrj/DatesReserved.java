f4a47742e5fafeff9ef2689a638dcd052753da00
Nate Johnson <natej1@live.com>
2017-11-02 13:13:39 -0400

    some things

--- src/CampingPrj/DatesReserved.java
+++ src/CampingPrj/DatesReserved.java
@@ -1,5 +1,6 @@
 package CampingPrj;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
@@ -17,16 +18,35 @@ public class DatesReserved {
 		}
 		
 		//This creates 20 years worth of boolean arraylists
-		for(int i = 0; i < 7300; i++) {
+		for(int i = 0; i < 14600; i++) {
 			dateList.add((ArrayList<ArrayList>)siteList.clone());
 		}
 	}
 	
-	public void reserve(int siteNum, GregorianCalendar date) {
+	private int dateToIndex(GregorianCalendar date) {
+		int dayOfYear = date.getInstance().get(date.DAY_OF_YEAR);
+		int year = date.getInstance().get(date.YEAR);
 		
+		switch(year) {
+		case 2015:
+			return dayOfYear;
+		case 2016:
+			return dayOfYear + 365;
+		case 2017:
+			return dayOfYear + 2*365;
+		case 2018:
+			return dayOfYear + 3*365;
+		default:
+			return -1;
+		}
+	}
+	
+	
+	public void reserve(int siteNum, GregorianCalendar date) {
+		dateList.get(dateToIndex(date)).set(siteNum -1, true);
 	}
 	
 	public void delete(int siteNum, GregorianCalendar date) {
-		
+		dateList.get(dateToIndex(date)).set(siteNum -1, false);
 	}
 }
